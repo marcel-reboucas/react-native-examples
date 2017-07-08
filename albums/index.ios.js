e91ed3bce0e747a2c0f5ac9bb8c9973dd0f6cbbc
Marcel Rebouças <marcel.reboucas@ubee.in>
2017-07-08 15:52:23 -0300

    Starting with react-native

---
+++ albums/index.ios.js
@@ -0,0 +1,13 @@
+
+// Import a library
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make the component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+// Make the component available
+export default Header;
