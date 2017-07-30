2baf54a4d606fe320c49efba560029f058eebf05
Marcel Rebouças <marcel.reboucas@ubee.in>
2017-07-30 14:56:42 -0300

    Setting up auth and login forms

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAdAPUAxT1Npa_11bKuQIP1_dnOuqMLhTQ",
+      authDomain: "react-native-auth-371ca.firebaseapp.com",
+      databaseURL: "https://react-native-auth-371ca.firebaseio.com",
+      projectId: "react-native-auth-371ca",
+      storageBucket: "react-native-auth-371ca.appspot.com",
+      messagingSenderId: "500493364629"
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text> An app! </Text>
+        <Header headerText="Authentication" /> 
+        <LoginForm />
       </View>
     );
   }
