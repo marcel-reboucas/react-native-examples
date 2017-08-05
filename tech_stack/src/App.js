b8b1e715e0acb1fd5e86bfbdb458faf4c6c9baed
Marcel Rebouças <marcel.reboucas@ubee.in>
2017-08-05 15:25:48 -0300

    Setting up redux

---
+++ tech_stack/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+import reducers from './reducers';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
