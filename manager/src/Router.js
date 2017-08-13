ae387809e1b9dac78d31762ae860da25398aead4
Marcel Rebouças <marcel.reboucas@ubee.in>
2017-08-13 10:37:51 -0300

    Adds the employee form.

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router} from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -10,13 +11,20 @@ const RouterComponent = () => {
         <Scene key="login" component={LoginForm} title="Please Login" />
       </Scene>
 
-      <Scene key="main">
+      <Scene key="main" initial>
         <Scene 
           key="employeeList" 
           component={EmployeeList} 
           title="Employees" 
           rightTitle="Add"
-          onRight={() => console.log("")}/>
+          onRight={() => Actions.employeeCreate()}
+          initial
+        />
+        <Scene 
+          key="employeeCreate" 
+          component={EmployeeCreate}
+          title="Employee Create"
+        />
       </Scene>
       
     </Router>
